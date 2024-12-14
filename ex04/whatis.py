df9d7b0e736c9f8cea6555c7013912634582c026
dtanigaw <dtanigaw@student.42.fr>
2024-12-14 19:02:09 +0100

    add ex4

--- ex04/whatis.py
+++ ex04/whatis.py
@@ -1,30 +1,21 @@
-import argparse
 import sys
 
-# Init parser
-parser = argparse.ArgumentParser()
-
-# Add arg to parser
-parser.add_argument(
-    "number", type=str, nargs='?',
-    help="The number to check whether it is odd or even.",
-    )
-
-# Parse args
-args = parser.parse_args()
-
+# Get args
+args = sys.argv[1:] # Exclude the script name
 # Check if there is more than one argument
-args_len = len(vars(args))
+args_len = len(args)
 
-if args_len <= 1: sys.exit()
+if args_len < 1:
+    sys.exit()
 if args_len > 1:
     raise AssertionError("more than one argument is provided")
 
 # Try to convert the argument to an integer
 try:
-    num = int(args.number)
+    num = int(args[0])
 except ValueError:
     raise AssertionError("argument is not an integer")
+    sys.exit(1)
 
 # Determine if the number is odd or even
 if num % 2 == 0:
