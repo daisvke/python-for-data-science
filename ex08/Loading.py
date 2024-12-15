85d8a5b8936d67703de59348c7dda3c89912f279
dtanigaw <dtanigaw@student.42.fr>
2024-12-15 23:46:30 +0100

    ex08: OK

--- ex08/Loading.py
+++ ex08/Loading.py
@@ -1,9 +1,11 @@
-from sys import stdout
 from time import time
+from shutil import get_terminal_size
 
 
 """
-Implementation of tqdm
+Implementation of tqdm from the tdqm package
+
+ - Handled option: unit, leave, delay, colour
 """
 
 def get_ansi_colours() -> dict:
@@ -25,7 +27,6 @@ def ft_tqdm(
     iterable: any, 
     unit: str="it",
     leave: bool=True,
-    initial: int|float=0,
     delay: float=0.0,
     colour: str="white"
     ) -> any:
@@ -34,50 +35,69 @@ def ft_tqdm(
 	Implementation of tqdm
     """
 
+    # Get terminal size
+    terminal_size = get_terminal_size()
+    # Get terminal width
+    terminal_width = terminal_size.columns
+
     total_len = len(iterable)
 	# Quit if iterable is empty
     if total_len == 0:
-        print("0it [00:00, ?it/s]")
-        return
+        print(f"\r0{unit} [00:00, ?{unit}/s]", end="")
 
     start_time = time()
 
     # Set colours
     colour_dict = get_ansi_colours()
-    ansi_colour = colour_dict[colour]
+    ansi_colour = colour_dict[colour.lower()]
     reset_colour = "\033[0m"
 
-    for i, item in enumerate(iterable, initial + 1):
-        bar_len = 40
+    for i, item in enumerate(iterable, start=1):
         progress = i / total_len
         percentage = int(progress * 100)
-
-        # Create the progress bar
-        filled_len = int(bar_len * progress)
-        empty_len = bar_len - filled_len
-        bar = f"{ansi_colour}█{reset_colour}" * filled_len + " " * empty_len
 
         elapsed_time = time() - start_time
         minutes, seconds = divmod(int(elapsed_time), 60)
         formatted_elapsed_time = f"{minutes:02}:{seconds:02}"
 
         # Create time info
-        total_time = elapsed_time / progress
+        total_time = elapsed_time / progress if progress > 0 else 0
         remaining_time = total_time - elapsed_time 
         minutes, seconds = divmod(int(remaining_time), 60)
         formatted_remaining_time = f"{minutes:02}:{seconds:02}"
 
         # Create speed info
-        speed = i / elapsed_time
+        speed = i / elapsed_time if elapsed_time > 0 else 0
         speed_info = f"{speed:.2f}{unit}/s"
 
-        # Contacenate all info
-        info = f" [{formatted_elapsed_time}<{formatted_remaining_time}, {speed_info}]"
+        # Contacenate all time info
+        info_time = f" [{formatted_elapsed_time}<{formatted_remaining_time}, {speed_info}]"
+
+        # Concatenate all iteration info
+        info_iteration = f"{i}/{total_len}"
+
+        """
+		Some caracteristics from the original tqdm that we have copied:
+		 - The whole visual module has to occupy 100% of the terminal's width.
+		 - The width of the bar itself varies according to the info printed
+		  just afterwards:
+           when the iteration value 'i' goes from 9 to 10 or from 99 to 100
+		   and gets an additional digit, the bar's width loses 1 char of width.
+		"""
+        # Progression bar width: terminal width - infos around bar
+        bar_len = terminal_width - 7 - len(info_time) - len(info_iteration)
+
+        # Create the progress bar
+        filled_len = int(bar_len * progress)
+        empty_len = bar_len - filled_len
+        bar = f"{ansi_colour}█{reset_colour}" * filled_len + " " * empty_len
 
         if delay == 0 or (delay and elapsed_time > delay):
-            stdout.write(f"\r{percentage}%|{bar}| {i}/{total_len}{info}")
-
-        if leave == False:
-            stdout.write("\r")
+        # The percentage number is right-aligned within a field of 3 spaces
+            print(f"\r{percentage:>3}%|{bar}| {info_iteration}{info_time}", end="")
 
         yield item
+
+    # If leave is set to False, then we remove the bar before quitting
+    if not leave:
+    	print("\r" + " " * terminal_width, end="\r")
--- ex08/tester.py
+++ ex08/tester.py
@@ -1,23 +1,80 @@
 from time import sleep
 from tqdm import tqdm
-from Loading import ft_tqdm
+from Loading import ft_tqdm, get_ansi_colours
+from shutil import get_terminal_size
 
+# Get terminal size
+terminal_size = get_terminal_size()
+# Get terminal width
+width = terminal_size.columns
 
-print("Custom tqdm:")
-for elem in ft_tqdm("", colour="cyan"):
+# Create separation line
+separation_line = '-' * width
+
+# Create labels to distinguish between custom and original tqdm
+ansi_colours = get_ansi_colours()
+yellow = ansi_colours["yellow"]
+cyan = ansi_colours["cyan"]
+reset_colour = "\033[0m"
+label_custom = f"[{yellow}CUSTOM{reset_colour}]"
+label_original = f"[{cyan}ORIGINAL{reset_colour}]"
+label_info = f"[{yellow}INFO{reset_colour}]"
+
+# Example with leave argument set to False
+print(f"{label_info} Example with leave argument set to False\n")
+print(label_custom)
+for elem in ft_tqdm("", leave=False):
     sleep(0.005)
 print()
-print("Original tqdm:")
-for elem in tqdm("", colour="green"):
+print(label_original)
+for elem in tqdm("", leave=False):
     sleep(0.005)
 
-print("----------------------------------------")
+print(separation_line)
 
-print("Custom tqdm:")
-for elem in ft_tqdm(range(333), colour="cyan"):
+# Example with an empty iterator and 'elem' as unit and colours
+print(f"{label_info} Example with empty iterator + 'elem' as unit + colours\n")
+print(label_custom)
+for elem in ft_tqdm("", colour="yellow", unit="elem"):
     sleep(0.005)
 print()
-print("Original tqdm:")
-for elem in tqdm(range(333), colour="green"):
+print(label_original)
+for elem in tqdm("", colour="cyan", unit="elem"):
     sleep(0.005)
+
+print(separation_line)
+
+# Example with a delay value
+print(f"{label_info} Example with a delay value\n")
+print(label_custom)
+for elem in ft_tqdm(range(400), delay=.5):
+    sleep(0.002)
+print()
+print(label_original)
+for elem in tqdm(range(400), delay=.5):
+    sleep(0.002)
+print()
+
+# Example with the same arguments
+print(f"{label_info} Example with the same arguments\n")
+print(label_custom)
+for elem in ft_tqdm(range(333), colour="white"):
+    sleep(0.005)
+print()
+print(label_original)
+for elem in tqdm(range(333), colour="white"):
+    sleep(0.005)
+print()
+
+print(separation_line)
+
+# Example with a long sleep time
+print(f"{label_info} Example with a long sleep time\n")
+print(label_custom)
+for elem in ft_tqdm(range(333), colour="white"):
+    sleep(0.05)
+print()
+print(label_original)
+for elem in tqdm(range(333), colour="white"):
+    sleep(0.05)
 print()
