94c16c82c1eefadf4dd93ae3a3f43dad4a1268a8
dtanigaw <dtanigaw@student.42.fr>
2024-12-15 19:17:54 +0100

    mandatory done

--- ex08/Loading.py
+++ ex08/Loading.py
@@ -1,6 +1,64 @@
-def get
+from sys import stdout
+from time import time
 
-def ft_tqdm(iterable: any) -> any:
-   for item in iterable:
-       yield item
-	   print()
+
+def get_ansi_colours() -> dict:
+    return {
+        "black": "\033[30m",
+        "red": "\033[31m",
+        "green": "\033[32m",
+        "yellow": "\033[33m",
+        "blue": "\033[34m",
+        "magenta": "\033[35m",
+        "cyan": "\033[36m",
+        "white": "\033[37m"
+    }
+
+def ft_tqdm(
+    iterable: any, 
+    unit: str="it",
+    leave: bool=True,
+    initial: int|float=0,
+    delay: float=0.0,
+    colour: str="white"
+    ) -> any:
+
+    total_len = len(iterable)
+    start_time = time()
+
+    colour_dict = get_ansi_colours()
+    ansi_colour = colour_dict[colour]
+    reset_colour = "\033[0m"
+
+    for i, item in enumerate(iterable, initial + 1):
+        bar_len = 40
+        progress = i / total_len
+        filled_len = int(bar_len * progress)
+        empty_len = bar_len - filled_len
+
+        bar = f"{ansi_colour}█{reset_colour}" * filled_len + " " * empty_len
+
+        percentage = int(progress * 100)
+        elapsed_time = time() - start_time
+        minutes, seconds = divmod(int(elapsed_time), 60)
+        formatted_elapsed_time = f"{minutes:02}:{seconds:02}"
+
+        total_time = elapsed_time / progress
+        remaining_time = total_time - elapsed_time 
+        minutes, seconds = divmod(int(remaining_time), 60)
+        formatted_remaining_time = f"{minutes:02}:{seconds:02}"
+
+        speed = i / elapsed_time
+        info = ""
+
+        speed_info = f"{speed:.2f}{unit}/s"
+            
+        info = f" [{formatted_elapsed_time}<{formatted_remaining_time}, {speed_info}]"
+
+        if delay == 0 or (delay and elapsed_time > delay):
+            stdout.write(f"\r{percentage}%|{bar}| {i}/{total_len}{info}")
+
+        if leave == False:
+            stdout.write("\r")
+
+        yield item
--- ex08/tester.py
+++ ex08/tester.py
@@ -3,10 +3,9 @@ from tqdm import tqdm
 from Loading import ft_tqdm
 
 
-for elem in ft_tqdm(range(333)):
+for elem in ft_tqdm(range(333), colour="cyan"):
     sleep(0.005)
-    print(elem)
 print()
-for elem in tqdm(range(333)):
+for elem in tqdm(range(333), colour="green"):
     sleep(0.005)
 print()
