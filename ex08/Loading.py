47c68e8f32fa18fd3761a05c137b573442a6ada1
dtanigaw <dtanigaw@student.42.fr>
2024-12-15 19:21:59 +0100

    Add doc

--- ex08/Loading.py
+++ ex08/Loading.py
@@ -3,6 +3,9 @@ from time import time
 
 
 def get_ansi_colours() -> dict:
+    """
+    Returns a dictionary with all the ANSI basic colors
+    """
     return {
         "black": "\033[30m",
         "red": "\033[31m",
@@ -15,6 +18,8 @@ def get_ansi_colours() -> dict:
     }
 
 def ft_tqdm(
+    """
+    """
     iterable: any, 
     unit: str="it",
     leave: bool=True,
@@ -43,16 +48,17 @@ def ft_tqdm(
         minutes, seconds = divmod(int(elapsed_time), 60)
         formatted_elapsed_time = f"{minutes:02}:{seconds:02}"
 
+        # Create time info
         total_time = elapsed_time / progress
         remaining_time = total_time - elapsed_time 
         minutes, seconds = divmod(int(remaining_time), 60)
         formatted_remaining_time = f"{minutes:02}:{seconds:02}"
 
+        # Create speed info
         speed = i / elapsed_time
-        info = ""
-
         speed_info = f"{speed:.2f}{unit}/s"
-            
+
+        # Contacenate all info
         info = f" [{formatted_elapsed_time}<{formatted_remaining_time}, {speed_info}]"
 
         if delay == 0 or (delay and elapsed_time > delay):
